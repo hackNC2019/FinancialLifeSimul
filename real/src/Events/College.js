36256a29c48613d0c9c77ecc97b45d5341f75792
Sunnie Kwak <sunnie@live.unc.edu>
2019-10-12 19:42:05 -0400

    basic map to college

---
+++ real/src/Events/College.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+class College extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            college: ""
+        };
+    
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(type) {
+        this.setState({college: type})
+    }
+
+    render() {
+        if(this.state.college == "uni"){
+
+        }
+        if(this.state.college == "cc"){
+
+        }
+        if(this.state.college == "trade"){
+
+        }
+        if(this.state.college == "none"){
+
+        }
+        else{
+            return(
+                <div>
+                    <h1>Choose a College</h1>
+                    <Button onClick={() =>this.handleClick("uni")}>4 Year University</Button>
+                    <Button onClick={() =>this.handleClick("cc")}>Community College</Button>
+                    <Button onClick={() =>this.handleClick("trade")}>Trade School</Button>
+                    <Button onClick={() =>this.handleClick("none")}>No University</Button>
+                </div>
+            );
+        }
+        
+    
+    }
+
+}
+
+export default College;
--- real/src/Main.js
+++ real/src/Main.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import College from './Events/College'
 
 class Main extends React.Component{
-    handleClick = () => {
-        console.log("main")
-      }
+    constructor(props) {
+        super(props);
+        this.state = {clicked: false};
     
-      render() {
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick = () => {
+        this.setState({clicked: true})
+    }
+    
+    render() {
+        if(!this.state.clicked) {
         return (
             <div className="App">
                 Our Game ! Whatever it is called...
-            <br />
-            <Button onClick={this.handleClick}> Get Started </Button>
-          </div>
+                <br />
+                <Button onClick={this.handleClick}> Get Started </Button>
+            </div>
         );
-      }
+        }
+        else{
+        return (
+            <College />
+        )
+        }
+    
+    }
 
 }
 
