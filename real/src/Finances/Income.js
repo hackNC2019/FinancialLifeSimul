a06211157910b8063352a97ed297b53d00efd6f5
Sunnie Kwak <sunnie@live.unc.edu>
2019-10-13 00:46:13 -0400

    fixed yashs ugly ass boxes

--- real/src/Finances/Income.js
+++ real/src/Finances/Income.js
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import './FinancialPanel.css'
 
 class Income extends React.Component{
     constructor(props){
@@ -16,39 +17,16 @@ class Income extends React.Component{
         }
     }
     render(){
-        const classes = {
-            card: {
-                maxWidth: 345,
-            },
-            media: {
-                height: 140,
-            }
-          };
-        return(
-        <Card style={classes.card}>
-        <CardActionArea>
-          <CardMedia
-            image="https://img.icons8.com/plasticine/400/000000/economic-improvement.png"
-            title="Income"
-            style= {classes.media}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
+      return(
+        <Card className= "finstatus">
+          <CardContent id = "statuscontent">
+            <Typography gutterBottom variant="h6" component="h2">
               Income
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              Your current amount of Income: ${this.state.value}
+              ${this.state.value}
             </Typography>
           </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Do something
-          </Button>
-          <Button size="small" color="primary">
-            Do something Else
-          </Button>
-        </CardActions>
       </Card>
         );
     }
